8b0af5462deef1a56477305b108096cf740640bc
Mateo Florido <mateo.florido@canonical.com>
2023-02-21 09:06:08 -0500

    Added IPv4 CIDR/Mask Patch

--- src/cilium_manifests.py
+++ src/cilium_manifests.py
@@ -2,14 +2,31 @@
 
 from typing import Dict
 
-from ops.manifests import ConfigRegistry, ManifestLabel, Manifests
+from ops.manifests import ConfigRegistry, ManifestLabel, Manifests, Patch
+
+
+class SetIPv4CIDR(Patch):
+    """Configure IPv4 CIDR and Node Mask."""
+
+    def __call__(self, obj) -> None:
+        """Update ConfigMap IPv4 CIDR and Mask size."""
+        if not (obj.kind == "ConfigMap" and obj.metadata.name == "cilium-config"):
+            return
+
+        data = obj.data
+        data["cluster-pool-ipv4-cidr"] = self.manifests.config["cluster-pool-ipv4-cidr"]
+        data["cluster-pool-ipv4-mask-size"] = self.manifests.config["cluster-pool-ipv4-mask-size"]
 
 
 class CiliumManifests(Manifests):
     """Deployment manager for the Cilium charm."""
 
     def __init__(self, charm, charm_config):
-        manipulations = [ManifestLabel(self), ConfigRegistry(self)]
+        manipulations = [
+            ConfigRegistry(self),
+            ManifestLabel(self),
+            SetIPv4CIDR(self),
+        ]
 
         super().__init__("cilium", charm.model, "upstream/cilium", manipulations)
         self.charm_config = charm_config
